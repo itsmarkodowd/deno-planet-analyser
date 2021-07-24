0f403efd3d6e47a9d24d0a9adf48243b6f0fad47
itsmarkodowd <16547497+itsmarkodowd@users.noreply.github.com>
2021-07-24 12:11:50 +0100

    initial: analyse csv

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to run
+
+- Have deno installed
+
+`deno run --allow-read mod.ts`
---
+++ deps.ts
@@ -0,0 +1,7 @@
+// Standard library dependencies
+export { join } from "https://deno.land/std/path/mod.ts";
+export { parse } from "https://deno.land/std/encoding/csv.ts";
+export { BufReader } from "https://deno.land/std@0.102.0/io/bufio.ts";
+
+// Third party dependencies
+export { pick } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
